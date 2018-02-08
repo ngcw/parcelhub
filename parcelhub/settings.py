7143c8c116b58e8c4f5b45dc5dd40f0a4153c991
ngcw <chin.unisocial@gmail.com>
2018-02-08 21:42:33 +0800

    add new requirements

--- parcelhub/settings.py
+++ parcelhub/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu',
+    #'menu',
     'django_tables2',
     'import_export',
 ]
